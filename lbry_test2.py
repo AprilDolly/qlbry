d97a4aaecddb05076e4436d511fb32ad9c1da726
April Dolly <aprildolly@mail.com>
2022-04-16 17:09:06 -0700

    first thingy

---
+++ lbry_test2.py
@@ -0,0 +1,234 @@
+#!/usr/bin/python3
+#making Claim a sublcass of dict this time, so it isn't as shitty
+import requests
+import subprocess
+from subprocess import DEVNULL,PIPE
+import json
+import random
+
+class Account:
+	def __init__(self,dic,lbry):
+		print(dic)
+		print(dic.keys())
+		self.__dict__.update(dic)
+		self.lbry=lbry
+
+class DictWrapper:
+	def __init__(self,d):
+		self.update(d)
+
+class Claim(dict):
+	#may just turn into a dict
+	"""
+	PARAMETERS:
+	#USEFUL:
+	address: idk
+	amount: amount staked in the claim by the creator
+	canonical_url: the url of the claim
+	claim_id: idk lel
+	name: the name of the video or channel or whatever
+	
+	"""
+	def __init__(self,claim_id,name,lbry,dic={}):
+		super().__init__()
+		self.update(dic)
+		if not "name" in self.keys():
+			self["name"]=name
+		self.get_channel()
+		self.lbry=lbry
+		self.id=claim_id
+		self.name=name
+		self._resolved=False
+	def __getattr__(self,attr):
+		#treats key, value as attribute
+		try:
+			return self[attr]
+		except KeyError:
+			if True:
+				return ""
+			else:
+				try:
+					self.resolve()
+					return self[attr]
+				except KeyError:
+					self.get()
+					return self[attr]
+	def channel_claims(self):
+		target=None
+		if "signing_channel" in self.keys():
+			target=self["signing_channel"]["name"]
+		elif self["value_type"]=="channel":
+			target=self
+		if target!=None:
+			for c in self.lbry.get_channel_claims(target):
+				yield c
+		else:
+			yield None
+	def get_channel(self):
+		if "signing_channel" in self.keys():
+			if type(self["signing_channel"])==dict:
+				sc_dict=self["signing_channel"]
+				self["signing_channel"]=Claim(sc_dict["claim_id"],sc_dict["name"],self.lbry,dic=sc_dict)
+	def resolve(self):
+		if self._resolved:
+			return
+		if self.id in self.lbry.resolved_claims.keys():
+			self.update(self.lbry.resolved_claim_data[self["name"]])
+		else:
+			print("resolving",self.name,self.id)
+			try:
+				newdata=self.lbry.resolved_claim_data[self.name]
+			except KeyError:
+				self.update(self.lbry.resolve(self["name"]))
+		self.get_channel()
+
+	def get(self):
+		self.update(self.lbry.get(self))
+	def get_comments(self,offset):
+		pass
+		#https://comments.odysee.com/api/v2?m=comment.List
+	def to_dict(self):
+		dic={}
+		for k in self.keys():
+			dic[k]=self[k]
+		return dic
+			
+				
+		
+		
+class LBRY:
+	def __init__(self,lbrynet_name="lbrynet",*args,**kwargs):
+		self.lbrynet_name=lbrynet_name
+		self.lbrynet_exists=self.test_lbrynet()
+		self.resolved_claim_data={}
+		self.resolved_claims={}
+	def test_lbrynet(self):
+		try:
+			cmd_process=subprocess.Popen([self.lbrynet_name,"status"],stdout=PIPE,stderr=PIPE)
+			out=cmd_process.stdout.read().decode('utf-8')
+			if "Could not connect to daemon. Are you sure it's running?" in out:
+				return False
+			else:
+				return True
+		except FileNotFoundError:
+			return False
+	def start(self):
+		self.lbrynet_process=subprocess.Popen([self.lbrynet_name,'start'],stdout=PIPE,stderr=PIPE)
+		while True:
+			line=self.lbrynet_process.stderr.readline().decode('utf-8')
+			print(line)
+			if 'Done setting up file manager' in line:
+				return
+			elif "finished shutting down" in line:
+				return
+	def stop(self):
+		self.lbrynet_command('stop')
+		
+		
+	def get_channel_claims(self,channel):
+		if type(channel)==str:
+			channel={"name":channel}
+		rsp=self.lbrynet_command("claim","search","--channel={}".format(channel["name"]),"--order_by=release_time")
+		#print(rsp)
+		for j in rsp["items"]:
+			yield Claim(j["claim_id"],j["name"],self,dic=j)
+		total_pages=rsp["total_pages"]
+		if total_pages>1:
+			for i in range(2,total_pages+1):
+				rsp2=self.lbrynet_command("claim","search","--channel={}".format(channel["name"]),"--page={}".format(i),"--order_by=release_time")
+				for j in rsp2["items"]:
+					yield Claim(j["claim_id"],j["name"],self,dic=j)
+		
+		
+	def get(self,claim):
+		if self.lbrynet_exists:
+			return self.lbrynet_command("get",claim["name"])
+		else:
+			purl="https://api.na-backend.odysee.com/api/v1/proxy?m=get"
+			uri="lbry://{}".format(claim["name"])
+			pdata={"id":random.randrange(10,100000),"jsonrpc":"2.0","method":"get","params":{"uri":uri,"save_file":"false"}}
+			rsp=requests.post(purl,data=json.dumps(pdata))
+			
+			try:
+				return rsp.json()["result"]
+			except:
+				print(print(rsp.json()))
+	def lbrynet_command(self,*args,args_list=[]):
+		args=[self.lbrynet_name]+list(args)+args_list
+		cmd_process=subprocess.Popen(args,stdout=PIPE,stderr=PIPE)
+		out=cmd_process.stdout.read().decode('utf-8')
+		return json.loads(out)
+		
+	def get_accounts(self):
+		accounts=[]
+		accounts_json=self.lbrynet_command('account','list')
+		for acc in accounts_json['items']:
+			accounts.append(Account(acc,self))
+	def resolve(self,claim_name):
+		print("resolving crap")
+		if self.lbrynet_exists:
+			return self.lbrynet_command("resolve",claim_name)
+		else:
+			purl="https://api.na-backend.odysee.com/api/v1/proxy?m=resolve"
+			pdata={"jsonrpc":"2.0","method":"resolve","params":{"urls":["lbry://{}".format(claim_name)],"include_is_my_output":True,"include_purchase_receipt":True}}
+			return requests.post(purl,data=json.dumps(pdata)).json()["result"]
+	def resolve_batch(self,claims):
+		if self.lbrynet_exists:
+			names=[]
+			for c in claims:
+				#print(c)
+				names.append(c["name"])
+			resolved=self.lbrynet_command("resolve",args_list=names)
+			for k in resolved.keys():
+				for c in claims:
+					if c["name"] in k:
+						self.resolved_claim_data[c["name"]]=resolved[k]
+						break
+			#print(self.resolved_claim_data)
+		else:
+			claim_urls=[]
+			for c in claims:
+				claim_urls.append("lbry://{}".format(c.name))
+			purl="https://api.na-backend.odysee.com/api/v1/proxy?m=resolve"
+			pdata={"jsonrpc":"2.0","method":"resolve","params":{"urls":claim_urls,"include_is_my_output":True,"include_purchase_receipt":True}}
+			resolved= requests.post(purl,data=json.dumps(pdata)).json()["result"]
+			self.resolved_claim_data.update(resolved)
+		results_resolved=[]
+		for k in self.resolved_claim_data.keys():
+			for c in claims:
+				if c["name"] in k:
+					results_resolved.append(Claim("","",self,dic=self.resolved_claim_data[k]))
+					break
+		return results_resolved
+			
+	def search(self,query,mode='odysee',size=20,offset=0,nsfw=False,resolve_results=True,**kwargs):
+		print("searching for:{}".format(query))
+		if mode=='lbry' and self.lbrynet_exists:
+			r_json=self.lbrynet_command('claim','search',query)
+			results=[]
+			for item in r_json['items']:
+				results.append(Claim("","",self,dic=item))
+			return results
+		elif mode=='odysee' or self.lbrynet_exists==False:
+			results=[]
+			if nsfw or nsfw=="true":
+				nsfw='true'
+			else:
+				nsfw='false'
+			r=requests.get('https://lighthouse.odysee.com/search?s={}&size={}&from={}&nsfw={}'.format(query,size,offset,nsfw))
+			results=[]
+			for j in json.loads(r.text):
+				#print(r.text)
+				results.append(Claim(j["claimId"],j["name"],self))
+			if resolve_results:
+				return self.resolve_batch(results)
+			return results
+	def __getattr__(self,attr):
+		if attr=='cmd':
+			return self.lbrynet_command
+			
+if __name__=='__main__':
+	lbry=LBRY(lbrynet_name="lbrynet")
+	lbry.test_lbrynet()
+	for item in lbry.get_channel_claims("@aprildolly"):
+		print(item)
---
+++ qlbry3.py
@@ -0,0 +1,431 @@
+#!/usr/bin/python3
+from lbry_test2 import*
+from PySide2.QtWidgets import *
+from PySide2.QtGui import*
+from PySide2.QtCore import*
+from PySide2 import QtGui
+from PySide2.QtMultimedia import *
+from PySide2.QtMultimediaWidgets import *
+from PySide2.QtMultimediaWidgets import *
+import urllib
+import sys
+import _thread as thread
+import numpy as np
+def nothing(*args):
+	pass
+
+class DownloadImageThread(QThread):
+	#thread that downloads an image and returns a pixmap
+	pixmap=Signal(QPixmap)
+	def __init__(self,img_url,parent=None):
+		super().__init__(parent)
+		self.img_url=img_url
+	def run(self):
+		pm=QPixmap()
+		try:
+			pm.loadFromData(urllib.request.urlopen(self.img_url).read())
+		except:
+			pass
+		self.pixmap.emit(pm)
+
+class FunctionAsQThread(QThread):
+	function_complete=Signal(tuple)
+	def __init__(self,function,*args,parent=None):
+		super().__init__(parent)
+		self.function=function
+		self.args=args
+	def run(self):
+		result=(self.function(*self.args),)
+		self.function_complete.emit(result)
+
+class IteratorAsQThread(QThread):
+	iterate=Signal(tuple)
+	def __init__(self,function,*args,parent=None):
+		super().__init__(parent)
+		print("PARENT:",self.parent())
+		self.function=function
+		self.args=args
+		self.is_running=False
+	def run(self):
+		self.is_running=True
+		for item in self.function(*self.args):
+			if self.is_running:
+				self.iterate.emit((item,))
+			else:
+				break
+		
+class NotAFunction:
+	def __init__(self,function,*params):
+		self.function=function
+		self.params=params
+	def __call__(self):
+		return self.function(*self.params)
+
+class SearchResultWidget(QPushButton):
+	def __init__(self,claim,parent=None,minimum_width=700,minimum_height=110):
+		self.claim=claim
+		super().__init__(parent)
+		self.setMinimumHeight(minimum_height)
+		self.setMinimumWidth(minimum_width)
+		self.threaded_init()
+	def purge(self):
+		self.thread.quit()
+		self.thread.wait()
+		self.deleteLater()
+	
+	def add_image(self,pm):
+		#pm=pm.scaledToWidth(100)
+		#pm=pm.scaledToHeight(100)
+		self.img.setPixmap(pm)
+		pm_size=pm.size()
+		pm_size.scale(100,100,Qt.AspectRatioMode.KeepAspectRatio)
+		self.img.setMaximumSize(pm_size)
+	
+	def threaded_init(self):
+		#layout containing thumbnail and text
+		#print(self.claim.__dict__)
+		h_layout=QHBoxLayout()
+		h_layout.setAlignment(Qt.AlignLeft)
+		
+		#image
+		try:
+			img_url=self.claim["value"]["thumbnail"]['url']
+			self.thread=DownloadImageThread(img_url,parent=self)
+			self.thread.pixmap.connect(self.add_image)
+			self.thread.start()
+		except:
+			#no image for now. lool
+			pass
+		self.img=QLabel(self)
+		self.img.setScaledContents(True)
+		h_layout.addWidget(self.img)
+		
+		#layout for text elements (content title, channel name, etc)
+		self.txt_layout_widget=QWidget(self)
+		txt_layout=QVBoxLayout()
+		
+		#content title
+		self.title_label=QLabel(self)
+		self.title_label.setText(self.claim["value"]["title"])
+		txt_layout.addWidget(self.title_label)
+		
+		#channel title
+		if self.claim["value_type"]!="channel":
+			try:
+				self.channel_label=QLabel(self)
+				self.channel_label.setText(self.claim["signing_channel"]["value"]["title"])
+				txt_layout.addWidget(self.channel_label)
+			except:
+				#this keeps giving recursion errors, ugh
+				pass
+		
+		#add stuff together
+		self.txt_layout_widget.setLayout(txt_layout)
+		h_layout.addWidget(self.txt_layout_widget)
+		self.setLayout(h_layout)
+		
+class StreamPlayerWidget(QWidget):
+	def __init__(self,*args,autoplay=False):
+		if len(args)>1:
+			#first arg is parent, second is claim
+			parent=args[0]
+			self.claim=args[1]
+			super().__init__(parent)
+		else:
+			self.claim=args[0]
+			super().__init__()
+		self.claim.get()
+		self.playing=False
+		#vertical layout including player and transport controls container
+		v_layout=QGridLayout()
+		
+		#stream player widget, be it audio or video
+		if self.claim["value"]["stream_type"]=="video":
+			#video stream
+			print("VIDEO STREAM DETECTED")
+			self.player_widget=QVideoWidget(self)
+			self.player_widget.setSizePolicy(QSizePolicy.Minimum,QSizePolicy.Expanding)
+			self.player=QMediaPlayer(None,QMediaPlayer.VideoSurface)
+			self.player.setVideoOutput(self.player_widget)
+			v_layout.addWidget(self.player_widget,0,0)
+			self.stream_info=self.claim["value"]["video"]
+		elif self.claim["value"]["stream_type"]=="audio":
+			#audio not yet supported
+			self.player=QMediaPlayer(None)
+			self.stream_info=self.claim["value"]["audio"]
+			pass
+		self.player.positionChanged.connect(self.position_changed)
+		self.player.durationChanged.connect(self.duration_changed)
+		self.player.setMedia(QMediaContent(QUrl(self.claim["streaming_url"])))
+		self.player.setNotifyInterval(100)
+		#container widget for transport controls
+		self.transport=QWidget(self)
+		transport_layout=QHBoxLayout()
+		self.playpause_button=QPushButton(self.transport)
+		self.playpause_button.setIcon(self.style().standardIcon(QStyle.SP_MediaPlay))
+		self.playpause_button.clicked.connect(self.play_or_pause)
+		transport_layout.addWidget(self.playpause_button)
+		self.pos_slider=QSlider(Qt.Horizontal,parent=self.transport)
+		self.pos_slider.setRange(0,self.stream_info["duration"])
+		transport_layout.addWidget(self.pos_slider)
+		self.transport.setLayout(transport_layout)
+		
+		#add everything together
+		v_layout.addWidget(self.transport,1,0)
+		self.setLayout(v_layout)
+		self.playing=False
+		self.media_set=False
+		self.pos_to_go_to=-1
+		self.current_pos=0
+		if autoplay:
+			self.play_or_pause()
+			
+		
+	def position_changed(self,pos):
+		if self.playing:
+			self.current_pos=pos
+		try:
+			if not self.pos_slider.isSliderDown():
+				if self.pos_to_go_to==-1:
+					self.pos_slider.setValue(pos/1000)
+					self.pos_to_go_to=-1
+				else:
+					#set position of player
+					self.player.setPosition(self.pos_to_go_to)
+					self.pos_to_go_to=-1
+			else:
+				self.pos_to_go_to=self.pos_slider.value()*1000
+		except AttributeError:
+			pass
+	def duration_changed(self,*args):
+		pass
+	def play_or_pause(self,*args):
+		if self.playing:
+			self.playing=False
+			self.player.pause()
+			self.playpause_button.setIcon(self.style().standardIcon(QStyle.SP_MediaPlay))
+		else:
+			print(self.player.position())
+			self.player.play()
+			#self.player.setPosition(self.current_pos)
+			self.playing=True
+			self.playpause_button.setIcon(self.style().standardIcon(QStyle.SP_MediaPause))
+		
+class ChannelWidget(QWidget):
+	claim_selected=Signal(Claim)
+	def __init__(self,channel,parent=None):
+		super().__init__(parent)
+		self.channel=channel
+		main_vbox=QVBoxLayout()
+		#main box will have a content widget (will contain all claims in channel)
+		
+		#this will contain channel picture, banner, title, etc
+		self.header=QWidget(self)
+		header_layout=QHBoxLayout()
+		
+		#channel image
+		self.channel_img=QLabel(self)
+		self.channel_img.setScaledContents(True)
+		#do a threaded image download like the others
+		img_url=self.channel["value"]["thumbnail"]["url"]
+		self.c_img_thread=DownloadImageThread(img_url,parent=self)
+		self.c_img_thread.pixmap.connect(self.add_c_image)
+		self.c_img_thread.finished.connect(nothing)
+		self.c_img_thread.start()
+		header_layout.addWidget(self.channel_img)
+		
+		#text containing channel title
+		self.title_text=QLabel(self)
+		self.title_text.setText(self.channel["value"]["title"])
+		header_layout.addWidget(self.title_text)
+		self.header.setLayout(header_layout)
+		
+		#footer, might have a tab system eventually
+		self.footer=QWidget(self)
+		footer_layout=QVBoxLayout()
+		
+		self.claim_list=QWidget()
+		self.claim_list.setSizePolicy(QSizePolicy.Maximum,QSizePolicy.Expanding)
+		self.claim_list_layout=QVBoxLayout()
+		self.claim_list.setLayout(self.claim_list_layout)
+		footer_layout.addWidget(self.claim_list)
+		self.footer.setLayout(footer_layout)
+		
+		#add everything
+		main_vbox.addWidget(self.header)
+		main_vbox.addWidget(self.footer)
+		self.setLayout(main_vbox)
+		self.load_claims()
+	def load_claims(self):
+		self.claim_search_thread=IteratorAsQThread(self.channel.channel_claims,parent=self)
+		self.claim_search_thread.iterate.connect(self.render_claim)
+		self.claim_search_thread.start()
+	def add_c_image(self,pm):
+		self.channel_img.setPixmap(pm)
+		pm_size=pm.size()
+		pm_size.scale(100,100,Qt.AspectRatioMode.KeepAspectRatio)
+		self.channel_img.setMaximumSize(pm_size)
+	
+	def render_claim(self,claim):
+		claim_to_add,=claim
+		cw=SearchResultWidget(claim_to_add,parent=self)
+		cw.setSizePolicy(QSizePolicy.Maximum,QSizePolicy.Expanding)
+		cw.clicked.connect(NotAFunction(self.claim_selected.emit,claim_to_add))
+		self.claim_list_layout.addWidget(cw)
+	def purge(self):
+		self.claim_search_thread.quit()
+		self.claim_search_thread.wait()
+		self.c_image_thread.quit()
+		self.c_image_thread.wait()
+		self.deleteLater()
+		
+class LBRYClient(QMainWindow):
+	def __init__(self,lbrynet_name="lbrynet",start_lbrynet=True):
+		super().__init__()
+		self.lbry=LBRY(lbrynet_name=lbrynet_name)
+		self.setWindowTitle("qlbry")
+		if start_lbrynet:
+			#progress bar and stuff
+			self.progress_bar_container=QWidget(self)
+			pb_layout=QVBoxLayout()
+			self.pb_label=QLabel(self.progress_bar_container)
+			self.pb_label.setText("Initializing lbrynet daemon, please wait")
+			pb_layout.addWidget(self.pb_label)
+			self.pb=QProgressBar(self.progress_bar_container)
+			self.pb.setMaximum(0)
+			self.pb.setMinimum(0)
+			pb_layout.addWidget(self.pb)
+			self.progress_bar_container.setLayout(pb_layout)
+			self.setCentralWidget(self.progress_bar_container)
+			self.lbry_start_thread=FunctionAsQThread(self.lbry.start,parent=self)
+			self.lbry_start_thread.function_complete.connect(self.on_lbry_start_thread_complete)
+			self.lbry_start_thread.start()
+		else:
+			self.initialize_main_widgets()
+	def on_lbry_start_thread_complete(self,args):
+		self.progress_bar_container.deleteLater()
+		self.initialize_main_widgets()
+	def initialize_main_widgets(self):
+		self.main_container=QWidget(self)
+		self.setCentralWidget(self.main_container)
+		main_container_layout=QGridLayout()
+		#search bar
+		self.nav_bar=QWidget(self)
+		nav_bar_layout=QGridLayout()
+		self.search_field=QLineEdit(self)
+		self.search_field.returnPressed.connect(self.search)
+		self.search_button=QPushButton(self)
+		self.search_button.setText("search")
+		self.search_button.clicked.connect(self.search)
+		nav_bar_layout.addWidget(self.search_field,0,0)
+		nav_bar_layout.addWidget(self.search_button,0,1)
+		self.nav_bar.setLayout(nav_bar_layout)
+		
+		#lower area
+		self.content_area=QScrollArea()
+		self.content_area.setWidgetResizable(True)
+		
+		#add everything together
+		main_container_layout.addWidget(self.nav_bar,0,0)
+		main_container_layout.addWidget(self.content_area,1,0)
+		self.main_container.setLayout(main_container_layout)
+	def clean_up_widgets(self):
+		#clean up any widgets and threads from earlier tasks
+		try:
+			self.search_thread.exit()
+			self.search_thread.wait()
+		except AttributeError:
+			pass
+		try:
+			self.current_channel.purge()
+		except AttributeError:
+			pass
+		except RuntimeError:
+			pass
+		try:
+			self.result_widgets[1]
+			for w in self.result_widgets:
+				try:
+					w.purge()
+				except:
+					pass
+			self.result_widgets=[]
+		except AttributeError:
+			pass
+		except IndexError:
+			pass
+		try:
+			self.claim_container.deleteLater()
+		except AttributeError:
+			pass
+		except RuntimeError:
+			pass
+		try:
+			self.stream_player.player.pause()
+			self.stream_player.delete_later()
+		except AttributeError:
+			pass
+		except RuntimeError:
+			pass
+		try:
+			self.content_area_container.deleteLater()
+		except AttributeError:
+			pass
+		except RuntimeError:
+			pass
+	def search(self):
+		self.clean_up_widgets()
+		self.current_query=self.search_field.text()
+		self.search_thread=FunctionAsQThread(self.threaded_search,self.current_query,parent=self)
+		self.search_thread.function_complete.connect(self.render_search_results)
+		self.search_thread.start()
+	def threaded_search(self,query):
+		print("started search")
+		#do actual searching
+		claims=self.lbry.search(query)
+		print("end search")
+		return claims
+	def render_search_results(self,results):
+		#generate search result widgets from each claim, add them to scroll area
+		claims,=results
+		self.result_widgets=[]
+		self.content_area_container=QWidget()
+		content_area_layout=QGridLayout()
+		self.content_area_container.setLayout(content_area_layout)
+		self.content_area.setWidget(self.content_area_container)
+		for i,claim in enumerate(claims):
+			print("adding")
+			w=SearchResultWidget(claim,self.content_area_container)
+			w.clicked.connect(NotAFunction(self.navigate_to_claim,claim))
+			self.result_widgets.append(w)
+			content_area_layout.addWidget(w,i,0)
+		print("search results rendered")
+		
+		
+
+	def navigate_to_claim(self,*args):
+		claim=args[0]
+		self.clean_up_widgets()
+		self.claim_container=QWidget()
+		claim_container_layout=QGridLayout()
+		if claim["value_type"]=="stream":
+			#audio or video
+			self.stream_player=StreamPlayerWidget(claim)
+			self.stream_player.setSizePolicy(QSizePolicy.Minimum,QSizePolicy.Expanding)
+			claim_container_layout.addWidget(self.stream_player,0,0)
+		elif claim["value_type"]=="channel":
+			#probably channel, i think
+			self.current_channel=ChannelWidget(claim)
+			self.current_channel.claim_selected.connect(self.navigate_to_claim)
+			claim_container_layout.addWidget(self.current_channel,0,0)
+			#print(claim.__dict__)
+			pass
+		self.claim_container.setLayout(claim_container_layout)
+		self.content_area.setWidget(self.claim_container)
+		self.claim_container.show()
+		
+if __name__=="__main__":
+	app=QApplication(sys.argv)
+	window=LBRYClient()
+	window.show()
+	app.exec_()
